6107d8ab4764fe446c6b8ac1ac62aa27943f7bf2
Valentina Andrade <valentina.vasquez.a@ug.uchile.cl>
2020-09-01 20:19:14 -0400

    model

--- processing/00proc-model.R
+++ processing/00proc-model.R
@@ -8,5 +8,16 @@ load(file = "../input/data/database2.RData")
 
 #3. Seleccionar variables
 names(db)
-db %>% select(country, year, fudi,ud,ud_fem2,ud_male2,rmw,gwg, T_GDPHRS_V, Coord,
-              sector_ser_TT)
+db1<- db %>% select(country, year,ud=UD,ud_fem2,ud_male2,rmw,gwg, T_GDPHRS_V,UR_WOMEN,UR_MEN, 
+              LFPR_WOMEN,LFPR_MEN, sector_ser_TT,sector_ser_FE, sector_ser_MA,sector_SER_SEX_T,sector_SER_SEX_F,sector_SER_SEX_M,
+              SH_PT_MW,SH_PT_WOMEN, SH_PT_MEN,
+              Coord,AdjCov) %>% mutate(fudi= ud_fem2/ud_male2, f_UR=UR_WOMEN/UR_MEN, f_LFPR=LFPR_WOMEN/LFPR_MEN,f_sector_ser = sector_ser_FE/sector_ser_MA,
+                                       f_sector_SER = sector_SER_SEX_F/sector_SER_SEX_M,
+                                       f_SH_PT = SH_PT_WOMEN/SH_PT_MEN) %>% filter(year >= 1970)
+
+
+# 4. Analisis casos perdidos
+summarytools::view(summarytools::dfSummary(db1), method = "viewer")
+
+# sector_ser_TT (Con más NA) --> sector_SER_SEX_T
+# AdjCov muchos NA
