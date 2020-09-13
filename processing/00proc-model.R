a27d830f6c84d213d3c67e5fe66c1338cb8c8b7f
Valentina Andrade <valentina.vasquez.a@ug.uchile.cl>
2020-09-13 19:50:31 -0300

    data-model
    Process data model
    - Final validation
    - Input by lofc
    - Fill Uruguay data

--- processing/00proc-model.R
+++ processing/00proc-model.R
@@ -1,7 +1,7 @@
 # Code 00: Proc-Model
 
 #1. Packages
-pacman::p_load(dplyr, tidyverse, zoo, googledrive, googlesheets4)
+pacman::p_load(WDI,dplyr, tidyverse, zoo, googledrive, googlesheets4)
 
 #2. Cargar base de datos
 load(file = "../input/data/database2.RData")
@@ -42,13 +42,63 @@ db1 <- db1 %>%  group_by(country) %>%
 
 impute <- function(x) mutate(x = ifelse(!is.na(fudi)&is.na(x), na.locf(x), x))         
 
-a <- db1[!complete.cases(db1),]
 
-gs_auth(new_user = TRUE)
+# 6. World Bank source
+
+# World Bank
+# Union density and sectors
+new_wdi_cache <- WDIcache() 
+WDIsearch("female.*unemployment.", cache = new_wdi_cache)
+
+db_aux <- WDI(indicator = c("SL.TLF.PART.FE.ZS",
+                            "SL.TLF.PART.MA.ZS",
+                            "SL.UEM.TOTL.FE.ZS",
+                            "SL.UEM.TOTL.MA.ZS",
+                            "SL.TLF.CACT.FE.ZS",
+                            "SL.TLF.CACT.MA.ZS",
+                            "SL.TLF.PART.FE.ZS", 
+                            "SL.TLF.PART.MA.ZS"), start = 1980, end = 2019, extra = TRUE)
+
+names(db_aux)
+db_aux <- db_aux %>% mutate(f_SH_PT_a= SL.TLF.PART.FE.ZS/SL.TLF.PART.MA.ZS,
+                            f_UR_a= SL.UEM.TOTL.FE.ZS/SL.UEM.TOTL.MA.ZS,
+                            f_sector_SER_a= SL.TLF.CACT.FE.ZS/SL.TLF.CACT.MA.ZS,
+                            f_LFPR_a= SL.TLF.PART.FE.ZS/SL.TLF.PART.MA.ZS) %>% select(country, year, region,f_SH_PT_a:f_LFPR_a)
+
+db1 <- db1 %>% merge(db_aux, by = c("country", "year"), all.x = T)  %>%
+  mutate(f_SH_PT = if_else(is.na(f_SH_PT), f_SH_PT_a, f_SH_PT),
+         f_UR = if_else(is.na(f_UR), f_UR_a, f_UR),
+         f_sector_SER = if_else(is.na(f_sector_SER), f_sector_SER_a, f_sector_SER),
+         f_LFPR = if_else(is.na(f_LFPR), f_LFPR_a, f_LFPR)) %>% select(-c(f_SH_PT_a:f_LFPR_a)) 
+
+db1 <- db1 %>% group_by(country) %>% fill(region, .direction= "up")
+# 7. Manual source
+
+db <- db1[!complete.cases(db1),]
+
+# --> To google sheets
+#(ss <- gs4_create("tesis", sheets = list(panel = db)))
+# head(db) %>% 
+#    sheet_write(ss, sheet = "complete")
+
+# 6.1 Read complete
+
+db <- read_sheet("https://docs.google.com/spreadsheets/d/1O4vdhArt6qPR3cV76cuTROomX7c97dbBK3w5Rq6Di2c/edit#gid=353438270")
 
 
-gs_new(title = "tesis", input = a)
+##6.2 Merge local with googleshetts
+db_model <- db1 %>% merge(db, by = c("country", "year", "fudi"), all.x = T)  %>%
+  mutate(f_SH_PT = if_else(is.na(f_SH_PT), f_SH_PT_a, na.locf(f_SH_PT)),
+         Coord = if_else(is.na(Coord), Coord_a, Coord),
+         f_UR = if_else(is.na(f_UR), f_UR_a, f_UR),
+         f_sector_SER = if_else(is.na(f_sector_SER), f_sector_SER_a, f_sector_SER),
+         f_LFPR = if_else(is.na(f_LFPR), f_LFPR_a, f_LFPR),
+         rmw = if_else(is.na(rmw), rmw_a, rmw),
+         T_GDPHRS_V = if_else(is.na(T_GDPHRS_V), T_GDPHRS_V_a, T_GDPHRS_V),
+         ud = if_else(is.na(ud), ud_a, ud),
+         AdjCov = if_else(is.na(AdjCov), AdjCov_a, AdjCov),
+         region = if_else(is.na(region), region_a, region)) %>%  select(-c(f_UR_a:region_a))  %>% group_by(country) %>% 
+  fill(f_SH_PT, .direction = "up")
 
-drive_mv("tesis", path = "~/R/Demo/")
-
-(ss <- gs4_create("fluffy-bunny", sheets = list(flowers = head(iris))))
+# Guardar para ocupar en modelos
+save(db_model, file = "../output/data/data-model.RData")
