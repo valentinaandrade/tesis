ba94f29f6b8d698166d7eb3bbe5f7edbbc97f1a7
Valentina Andrade <valentina.vasquez.a@ug.uchile.cl>
2020-09-13 11:58:43 -0300

    update proc model

--- processing/00proc-model.R
+++ processing/00proc-model.R
@@ -1,7 +1,7 @@
 # Code 00: Proc-Model
 
 #1. Packages
-pacman::p_load(dplyr, tidyverse, VIM)
+pacman::p_load(dplyr, tidyverse, zoo, googledrive, googlesheets4)
 
 #2. Cargar base de datos
 load(file = "../input/data/database2.RData")
@@ -13,7 +13,7 @@ db1<- db %>% select(country, year,ud=UD,ud_fem2,ud_male2,rmw,gwg, T_GDPHRS_V,UR_
               SH_PT_MW,SH_PT_WOMEN, SH_PT_MEN,
               Coord,AdjCov) %>% mutate(fudi= ud_fem2/ud_male2, f_UR=UR_WOMEN/UR_MEN, f_LFPR=LFPR_WOMEN/LFPR_MEN,f_sector_ser = sector_ser_FE/sector_ser_MA,
                                        f_sector_SER = sector_SER_SEX_F/sector_SER_SEX_M,
-                                       f_SH_PT = SH_PT_WOMEN/SH_PT_MEN) %>% filter(year >= 1970)
+                                       f_SH_PT = SH_PT_WOMEN/SH_PT_MEN) %>% filter(year >= 1970) 
 
 
 # 4. Analisis casos perdidos
@@ -21,3 +21,34 @@ summarytools::view(summarytools::dfSummary(db1), method = "viewer")
 
 # sector_ser_TT (Con más NA) --> sector_SER_SEX_T
 # AdjCov muchos NA
+#fudi 661 (corregir, deberían ser 661)
+# 1528 obs 
+
+#5. Imputation
+# impute multiple fudi by ud y LOFC
+
+db1 <- db1 %>% select(country, year, fudi, f_UR, f_LFPR, f_sector_SER, f_SH_PT, Coord, AdjCov, rmw, T_GDPHRS_V, ud)
+
+db1 <- db1 %>%  group_by(country) %>% 
+  mutate(f_UR = ifelse(!is.na(fudi)&is.na(f_UR), na.locf(f_UR), f_UR),
+         f_LFPR = ifelse(!is.na(fudi)&is.na(f_LFPR), na.locf(f_LFPR), f_LFPR),
+         f_SH_PT = ifelse(!is.na(fudi)&is.na(f_SH_PT), na.locf(f_SH_PT), f_SH_PT),
+         f_sector_SER = ifelse(!is.na(fudi)&is.na(f_sector_SER), na.locf(f_sector_SER), f_sector_SER),
+         Coord = ifelse(!is.na(fudi)&is.na(Coord), na.locf(Coord), Coord),
+         AdjCov = ifelse(!is.na(fudi)&is.na(AdjCov), na.locf(AdjCov), AdjCov),
+         rmw = ifelse(!is.na(fudi)&is.na(rmw), na.locf(rmw), rmw),
+         T_GDPHRS_V = ifelse(!is.na(fudi)&is.na(T_GDPHRS_V), na.locf(T_GDPHRS_V), T_GDPHRS_V),
+         ud = ifelse(!is.na(fudi)&is.na(ud), na.locf(ud), ud)) %>%  filter(!is.na(fudi))
+
+impute <- function(x) mutate(x = ifelse(!is.na(fudi)&is.na(x), na.locf(x), x))         
+
+a <- db1[!complete.cases(db1),]
+
+gs_auth(new_user = TRUE)
+
+
+gs_new(title = "tesis", input = a)
+
+drive_mv("tesis", path = "~/R/Demo/")
+
+(ss <- gs4_create("fluffy-bunny", sheets = list(flowers = head(iris))))
---
+++ processing/analysis-steps.md
@@ -0,0 +1,58 @@
+## Results
+
+### Descriptivos
+
+Antes de exponer el modelo multivariado, se exponen las tendencias de $FUDi$ de los X países analizados. Los patrones muestran  tres tipos de tendencias: 
+1. Crecimiento 
+2. Estancamiento
+3. Declive
+
+**Figuras**
+
+( --> Decir los n por cada uno, qué países destacan y cuándo <--)
+( Esto asociarlo a algunas cosas de las indicadas en el capítulo 2 )
+
+Aproximadamente 2 párrafos
+
+
+En la **Tabla** se pueden ver os estadísticos descriptivos para todas las variables utilizadas para el analisis. 
+
+Feminización de la densidad sindical --> media, minimo y máximo y para qué país. Luego cuándo fue el mayor lag de crecimiento y de declive.
+Variación feminización densdad sindical 
+
+Después cadauna de las variables independientes. 
+
+
+## Modelo
+
+Modelo Full
+
+Chequeo de robustez
+
+- Se ocupa Jackknife para ver casos influyentes*: esto son países que pueden estar influenciando mucho los resultados y a estos se les exluye del modelo
+
+- Modelo con países LA y el resto. 
+
+- Ir viendo hipótesis
+
+
+### Interacciones
+
+Modelo adicional donde se examina interacción entre modelo de relaciones laborales con variables economicas sobre la feminización de la densidad sindica. Se estomaron modelos separados paraconvsernar la estimación de cada interacción. El objetivo es ver como las estimaciones del modelo general son diferentes para países coordinados y liberlizados. 
+
+Ver el error correction rate y R2 para cada modelo
+
+Comparar como cambian con el resultado principal y luego la diferencia entre países
+
+Ir incorporando datos y experiencia de organizaciones según variable 
+
+
+
+## Robustez
+
+Especificación: T=2, N = 40-50 (los serios) --> Panel Data Econometrics by Badi Baltagi
+
+
+
+
+
